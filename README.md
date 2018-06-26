515f295fea21c6c7290afa7ddc63175ba1d8987a
ritesh kumar <kumar.ritesh97@hotmail.com>
2018-06-25 23:41:56 -0700

    Implemented dijktra algorithm

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Dial_a_Ride
 
-Java application for cab operators to provide economical transport service alongwith comfort of cab. 
-Jan 2018-Jun 2018
+Java application for cab operators to provide economical transport service alongwith comfort of cab.
--- dial_a_ride/src/com/hackercode/utilities/Dijktra.java
+++ dial_a_ride/src/com/hackercode/utilities/Dijktra.java
@@ -1,14 +1,13 @@
 package com.hackercode.utilities;
-import java.util.Scanner;
-public class Dijktras {
-    public static void dij(int graph[][],int n,int src) {
+public class Dijktra {
+    public  void dij(int location[][],int n,int src) {
 
-        int[] visited = new int[5];
+        int[] visited = new int[n];
         int inf = 999;
         int[] distance = new int[n];
-        int v = 0;
+        int v=0 ;
 
-        distance = graph[src];
+        distance = location[src];
         visited[src] = 1;
 
         distance[src] = 0;
@@ -25,14 +24,13 @@ public class Dijktras {
         visited[v] = 1;
         for (int j = 0;j < n; j++)
         {
-            if (((inf + graph[j][v]) < distance[j]) && (visited[j] != 1)) {
-                distance[j] = inf + graph[j][v];
+            if (((inf + location[j][v]) < distance[j]) && (visited[j] != 1)) {
+                distance[j] = inf + location[j][v];
 
             }
         }
 
-        for(int i=0;i<n;i++){
-            graph[src][i]=distance[i];
-        }
+        for(int i=0;i<n;i++) location[src][i] = distance[i];
+
     }
 }
